79670e2c19364213c936bc33345d18dbea4c913e
sakasaka0722 <61506294+sakasaka0722@users.noreply.github.com>
2020-12-27 22:27:35 +0900

    Add files via upload

---
+++ main-ja.js
@@ -0,0 +1,43 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['スヌーピー','うさまる','サンタクロース'];
+let insertY = ['北海道','ディズニーランド','沖縄'];
+let insertZ = ['突然歌い出した','道の真ん中で叫び出した','ミッキーになって踊っていた'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('梅子',name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature);
+    newStory = newStory.replace('300 pounds',weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
